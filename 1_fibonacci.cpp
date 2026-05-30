1d7006efd319a457cc3a33bb9119d1dde3bb4d21
Aditya Malik <adityamalik7973@gmail.com>
2026-05-30 15:51:02 +0530

    fibo using dp

--- 1_fibonacci.cpp
+++ 1_fibonacci.cpp
@@ -1,14 +1,22 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-int fibo(int n,vector<int> &dp){
+int fibo1(int n,vector<int> &dp){
     if(n<=1)return n;
     if(dp[n]!=-1)return dp[n];
-    return dp[n]=fibo(n-1,dp)+fibo(n-2,dp);
+    return dp[n]=fibo1(n-1,dp)+fibo1(n-2,dp);
+}
+int fibo2(int n,vector<int> &dp){
+    dp[0]=0;
+    dp[1]=1;
+    for(int i=2;i<=n;i++){
+        dp[i]=dp[i-1]+dp[i-2];
+    }
+    return dp[n];
 }
 int main(){
     int n;cin>>n;
     vector<int> dp(n+1,-1);
-    cout<<fibo(n,dp);
+    cout<<fibo2(n,dp);
     return 0;
 }
